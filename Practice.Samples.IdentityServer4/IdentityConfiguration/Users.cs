286cc781c80eb41b4b5ed1487d0994a15c36d8c1
rijan999 <rijanmaharjan111@gmail.com>
2024-05-31 16:30:46 +0545

    Added| Identity Server 4| Main Application

---
+++ Practice.Samples.IdentityServer4/IdentityConfiguration/Users.cs
@@ -0,0 +1,32 @@
+﻿using IdentityModel;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Practice.Samples.IdentityServer4.IdentityConfiguration
+{
+    public class Users
+    {
+        public static List<TestUser> Get()
+        {
+            return new List<TestUser> 
+            {
+                new TestUser 
+                {
+                    SubjectId = "56892347",
+                    Username = "gauravmaharjan",
+                    Password = "password",
+                    Claims = new List<Claim> 
+                    {
+                        new Claim(JwtClaimTypes.Email, "gauravmaharjan000@gmail.com"),
+                        new Claim(JwtClaimTypes.Role, "admin"),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.gauravmaharjan999.com.np/")
+                    }
+                }
+            };
+        }
+    }
+}
